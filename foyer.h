d4748559abe9597b7b64d4b2eaef958cf68efb60
Ray Smith <rqsmith@email.wm.edu>
2022-03-22 16:27:40 -0400

    Initial commit - display of FFoyer room and other support

---
+++ foyer.h
@@ -0,0 +1,21 @@
+
+#ifndef FOYER_H
+#define FOYER_H
+
+class Foyer
+{
+    private:
+        #Cabinet 
+        #Chair 
+        #Plant 1 
+        #Plant 2 
+        #Cabinet endtable1
+        #cabinet endtable2
+        #painting
+
+    public: 
+        void do_action (int time);
+
+
+
+}
