03d17f9dadd59522fb5cf27b8857ae5ce5f5805a
ShuhongWangWM <swang32@email.wm.edu>
2022-04-11 11:04:57 -0400

    update main function

--- bin/DevilsHour.cpp
+++ bin/DevilsHour.cpp
@@ -1,101 +1,102 @@
-//Using SDL and standard IO
+// Using SDL and standard IO
 #include <SDL.h>
 #include <SDL_image.h>
-#include <iostream>
 #include <stdio.h>
+
+#include <iostream>
+
 #include "eventManager.h"
 
-//Screen dimension constants
+// Screen dimension constants
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 768;
 
-//Room enummerations
-enum rooms {Front_Foyer, Bedroom, Bathroom, Kitchen, Hallway};
+// Room enummerations
+enum rooms { Front_Foyer, Bedroom, Bathroom, Kitchen, Hallway };
 
-void csci437_error(const std::string& msg)
-{
+void csci437_error(const std::string& msg) {
   std::cerr << msg << " (" << SDL_GetError() << ")" << std::endl;
   exit(0);
 }
 
-void csci437_img_error(const std::string& msg)
-{
+void csci437_img_error(const std::string& msg) {
   std::cerr << msg << " (" << IMG_GetError() << ")" << std::endl;
   exit(0);
 }
-SDL_Surface* load_bitmap( SDL_Surface* input ){
-    // Load bitmap
-    if(input == NULL) csci437_img_error("Could not image!");
-    return input;
+SDL_Surface* load_bitmap(SDL_Surface* input) {
+  // Load bitmap
+  if (input == NULL) csci437_img_error("Could not image!");
+  return input;
+}
 
-  }
-
-SDL_Texture* convert_image_to_texture(SDL_Renderer* renderer, SDL_Surface* image ){
+SDL_Texture* convert_image_to_texture(SDL_Renderer* renderer,
+                                      SDL_Surface* image) {
   // convert to texture
-  SDL_Texture* texture = SDL_CreateTextureFromSurface( renderer, image );
-  if(texture == NULL) csci437_error("Could not create texture from surface!");
-  
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
+  if (texture == NULL) csci437_error("Could not create texture from surface!");
+
   // delete image
-  SDL_FreeSurface( image );
+  SDL_FreeSurface(image);
   image = NULL;
 
   return texture;
 }
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
   /*** Initialization ***/
-  
+
   // Initialize SDL
-  if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) csci437_error("SDL could not initialize!");
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) csci437_error("SDL could not initialize!");
 
   // Create window
-  SDL_Window* window = SDL_CreateWindow( "Devil's Hour", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-  if( window == NULL ) csci437_error("Window could not be created!");
+  SDL_Window* window = SDL_CreateWindow("Devil's Hour", SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
+                                        SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (window == NULL) csci437_error("Window could not be created!");
 
   // Init Bitmap loading
-  if( IMG_Init( IMG_INIT_JPG | IMG_INIT_PNG ) < 0 ) csci437_img_error("SDL could not initialize bitmap loaders!");
+  if (IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) < 0)
+    csci437_img_error("SDL could not initialize bitmap loaders!");
 
   // Small delay to allow the system to create the window.
   SDL_Delay(100);
-    
+
   // Create renderer
-  SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+  SDL_Renderer* renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   if (renderer == NULL) csci437_error("Unable to create renderer!");
 
   SDL_Surface* image = load_bitmap(IMG_Load("../resource/FFoyerRoom.png"));
-  
+
   // convert to texture
-  SDL_Texture* texture = SDL_CreateTextureFromSurface( renderer, image );
-  if(texture == NULL) csci437_error("Could not create texture from surface!");
-  
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
+  if (texture == NULL) csci437_error("Could not create texture from surface!");
+
   // delete image
-  SDL_FreeSurface( image );
+  SDL_FreeSurface(image);
   image = NULL;
 
-  bool red=true, green=true, blue=true;
+  bool red = true, green = true, blue = true;
   float angle = 0;
-  
-  SDL_Rect dst = { (SCREEN_WIDTH - 256) / 2, (SCREEN_HEIGHT - 256) / 2, 256, 256};
+
+  SDL_Rect dst = {(SCREEN_WIDTH - 256) / 2, (SCREEN_HEIGHT - 256) / 2, 256,
+                  256};
   SDL_Point rot = {128, 128};
 
-  
   /*** Main Loop ***/
   bool running = true;
   EventMangager eventManager;
   SDL_Event e;
   rooms current_room;
   current_room = Front_Foyer;
-  Uint16 current_time = 0; 
+  Uint16 current_time = 0;
 
   // While application is running
-  while( running )
-  {
+  while (running) {
     // loop start time
     Uint32 start_ticks = SDL_GetTicks();
-    
+
     // Handle events on queue
-    while( SDL_PollEvent( &e ) != 0 )
-    {
+    while (SDL_PollEvent(&e) != 0) {
       // // User requests quit
       // if( e.type == SDL_QUIT ) running = false;
 
@@ -108,53 +109,50 @@ int main(int argc, char** argv)
       //   if( e.key.keysym.sym == SDLK_b ) current_room = Bedroom;
       //   if( e.key.keysym.sym == SDLK_k ) current_room = Kitchen;
       //   if( e.key.keysym.sym == SDLK_h ) current_room = Hallway;
-        Uint16 delta_time  = (start_ticks - current_time);
-        
-        eventManager.handle_event(e, delta_time, start_ticks, &running);
+      Uint16 delta_time = (start_ticks - current_time);
 
-
-      }
-      // current time 
-      current_time = SDL_GetTicks();
-
-      switch (current_room){
-        case Front_Foyer:
-          image = load_bitmap(IMG_Load("../resource/FFoyerRoom.png"));
-          texture = convert_image_to_texture(renderer, image);
-          break;
-        case Bedroom:
-          image = load_bitmap(IMG_Load("../resource/bitmap.png"));
-          texture = convert_image_to_texture(renderer,image);
-          break;
-      }
-
-      // render
-      SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
-      SDL_RenderClear( renderer );
-      SDL_SetTextureColorMod( texture, red*255, green*255, blue*255 );
-      SDL_RenderCopy( renderer, texture, NULL, NULL);
-      SDL_RenderPresent( renderer );
+      eventManager.handle_event(e, delta_time, start_ticks, &running);
     }
-    // Gets end time of the loop 
-    Unit32 end_time = SDL_GetTicks(); 
-    
+    // current time
+    current_time = SDL_GetTicks();
+
+    switch (current_room) {
+      case Front_Foyer:
+        image = load_bitmap(IMG_Load("../resource/FFoyerRoom.png"));
+        texture = convert_image_to_texture(renderer, image);
+        break;
+      case Bedroom:
+        image = load_bitmap(IMG_Load("../resource/bitmap.png"));
+        texture = convert_image_to_texture(renderer, image);
+        break;
+    }
+
+    // render
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetTextureColorMod(texture, red * 255, green * 255, blue * 255);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
   }
+  // Gets end time of the loop
+  Unit32 end_time = SDL_GetTicks();
+}
 
-  /*** Clean Up ***/
+/*** Clean Up ***/
 
-  // Destroy texture
-  SDL_DestroyTexture( texture );
-  
-  // Destroy renderer
-  SDL_DestroyRenderer( renderer );
-  
-  // Destroy window
-  SDL_DestroyWindow( window );
+// Destroy texture
+SDL_DestroyTexture(texture);
 
-  // Quit SDL subsystems
-  IMG_Quit();
-  SDL_Quit();
-  
-  // Done.
-  return 0;
+// Destroy renderer
+SDL_DestroyRenderer(renderer);
+
+// Destroy window
+SDL_DestroyWindow(window);
+
+// Quit SDL subsystems
+IMG_Quit();
+SDL_Quit();
+
+// Done.
+return 0;
 }
--- build/CMakeFiles/DevilsHour_core.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libDevilsHour_core.a"
-)
