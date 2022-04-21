7d42c7c4b70fb3ca73e7fd1238bf662abfc13e16
ShuhongWangWM <swang32@email.wm.edu>
2022-04-21 16:35:09 -0400

    update dialog.xml

--- include/furniture.h
+++ include/furniture.h
@@ -1,26 +1,28 @@
 
 #ifndef _FURNITURE_H_
-#define _FURNITURE_H_ 
+#define _FURNITURE_H_
 
-#include <vector> 
+#include <string>
+#include <vector>
+
+#include "entity.h"
 
 /**
- * @brief Allows the enviorment to handle  
+ * @brief Allows the enviorment to handle
  * user interactions
- * 
+ *
  */
 class Furniture : public Entity {
-    private:
-        std::vector<std::string> items; /**< stores the items the furniture(object) holds */
-        
-    public:
-        interacted() /**< handles the user interacting with furniture */
-        /**
-         * @brief  Furniture constructor
-         * 
-         */
+ private:
+  std::vector<std::string>
+      items; /**< stores the items the furniture(object) holds */
 
-        Furniture Furniture ();
-        
+ public:
+  int interacted(); /**< handles the user interacting with furniture */
+  /**
+   * @brief  Furniture constructor
+   *
+   */
+  Furniture();
 };
 #endif
