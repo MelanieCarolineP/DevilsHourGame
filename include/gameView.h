6d81a1050839376fb44c23f165302dcc3ea5d947
Ray Smith <rqsmith@email.wm.edu>
2022-04-29 18:53:32 -0400

    Manual local merging for gameView implementation

--- include/gameView.h
+++ include/gameView.h
@@ -3,30 +3,45 @@
 
 #include <iostream>
 
+#include "actor.h"
+#include "constants.h"
 #include "enum.h"
+#include "pauseMenu.h"
 #include "room.h"
 #include "spriteSheet.h"
+#include "stdio.h"
 #include "vec2d.h"
 
 class GameView {
  public:
   /* constructor */
-  GameView();
+  GameView(){};
+
+  GameView(SDL_Renderer *renderer);
 
   /* methods to draw element */
   void drawStartScreen(SDL_Renderer *renderer);
+  void displayGame(Actor *actor);
   void drawUI();
-  void drawRoom(SDL_Renderer *renderer, Room r);
-  void drawActor(SDL_Renderer *renderer, Vec2d position, Vec2d size,
-                 direction direction);
+  void drawRoom(Room *r, Rooms room_name);
+  void drawActor(Vec2d position, Vec2d size, direction direction);
   void drawDoor();
+  void drawPauseMenu(void);
+
   void drawInventory();
+  void presentScreen(void);
+  void clearScreen(void);
 
  private:
   /* helper function for drawInventory() */
   void drawItem();
   SpriteSheet sprite;
   SDL_Rect rect;
+  PauseMenu pauseMenu;
+  SDL_Renderer *renderer;
+  Room *currentR;
+  Rooms currentRoom;
+  SDL_Texture *roomTexture;
 };
 
 #endif /* _GAMEVIEW_H_ */
